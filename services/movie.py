3876a284f25bfd71ae4a3c2bf0cdfc1f016508dd
Marina <marina.ua13@gmail.com>
2024-01-30 22:58:57 +0200

    Solution4

--- services/movie.py
+++ services/movie.py
@@ -1,17 +1,17 @@
 from django.db.models import QuerySet
-from db.models import Movie, Genre, Actor
+from db.models import Movie
 
 
 def get_movies(
         genres_ids: list[int] = None,
-        actors_ids: list[int] = None) -> QuerySet | Movie:
+        actors_ids: list[int] = None) -> QuerySet:
     queryset = Movie.objects.all()
 
     if genres_ids and actors_ids:
         return queryset.filter(genres__in=genres_ids, actors__in=actors_ids)
-    elif genres_ids:
+    if genres_ids:
         return queryset.filter(genres__in=genres_ids)
-    elif actors_ids:
+    if actors_ids:
         return queryset.filter(actors__in=actors_ids)
 
     return queryset
@@ -24,20 +24,17 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(movie_title: str,
                  movie_description: str,
                  genres_ids: list[int] = None,
-<<<<<<< HEAD
                  actors_ids: list[int] = None) -> Movie:
-=======
-                 actors_ids: list[int] = None) -> QuerySet:
->>>>>>> origin/develop
+
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description
     )
 
     if genres_ids:
-        movie.genres.add(*Genre.objects.filter(id__in=genres_ids))
+        movie.genres.set(genres_ids)
 
     if actors_ids:
-        movie.actors.add(*Actor.objects.filter(id__in=actors_ids))
+        movie.actors.set(actors_ids)
 
     return movie
